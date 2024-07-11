88be35b3516679ccbd1f1af8a264cfccb61c6765
Mikel Jimenez <majc1907@gmail.com>
2024-07-11 14:14:26 +0200

    changes

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -2,28 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Producto } from '../../models/producto';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-  url = 'http://localhost:4000/api/productos/'
+  //url = 'http://localhost:4000/api/productos/'
+  private url = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getProductos(): Observable<any> {
-   return this.http.get(this.url);
+    return this.http.get(`${this.url}/api/productos`);
   }
   eliminarProducto(id: String): Observable<any> {
-    return this.http.delete(this.url + id);
+    return this.http.delete(`${this.url}/api/productos/${id}`);
   }
-  guardarProducto(producto: Producto): Observable<any>{
-return this.http.post(this.url, producto)
+  guardarProducto(producto: Producto): Observable<any> {
+    return this.http.post(`${this.url}/api/productos`, producto)
   }
-  obtenerProducto(id: String): Observable<any>{
-    return this.http.get(this.url + id);
+  obtenerProducto(id: String): Observable<any> {
+    return this.http.get(`${this.url}/api/productos/${id}`);
   }
-  editarProducto(id: string, producto: Producto): Observable<any>{
-    return this.http.put(this.url + id, producto);
+  editarProducto(id: string, producto: Producto): Observable<any> {
+    return this.http.put(`${this.url}/api/productos/${id}`, producto);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://server-angular.vercel.app/'
+  apiUrl: 'http://localhost:4000'
 };
